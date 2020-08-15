f1f0087468dbf3fa92faa60113cf49989e8cfd5c
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-08-14 22:38:01 -0500

    Updates

---
+++ precache-manifest.6abf14a3de014518d1c3eab58739d79d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1107bab3e7e6bcafb00f3f4e39a0b7aa",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e2e146ddda2cc2cef8f9",
+    "url": "/portfolio/static/css/main.03f1dbcc.chunk.css"
+  },
+  {
+    "revision": "40fc8ea52f4aa8546318",
+    "url": "/portfolio/static/js/2.1ea96fa5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1ea96fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2e146ddda2cc2cef8f9",
+    "url": "/portfolio/static/js/main.d0fe2e0c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "7721296adf80661179a2390068a484ec",
+    "url": "/portfolio/static/media/projectThumb.7721296a.png"
+  }
+]);
