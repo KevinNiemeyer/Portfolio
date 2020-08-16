21857ed1c530c3d6a262bef5486d077de4c96eeb
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-08-16 00:28:26 -0500

    Updates

---
+++ precache-manifest.4f91022de528804db5c79515a51c57bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b426e79a06f1c4b78d1fe4c7739db0",
+    "url": "/portfolio/npm/index.html"
+  },
+  {
+    "revision": "61eeb898291642b1a200",
+    "url": "/portfolio/npm/static/css/main.d114a778.chunk.css"
+  },
+  {
+    "revision": "25e3518aeaaf9f8ec978",
+    "url": "/portfolio/npm/static/js/2.65f412bb.chunk.js"
+  },
+  {
+    "revision": "651a700662a0bc6deb234061648434d5",
+    "url": "/portfolio/npm/static/js/2.65f412bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61eeb898291642b1a200",
+    "url": "/portfolio/npm/static/js/main.c49fc57a.chunk.js"
+  },
+  {
+    "revision": "2fd186b8db465764ac92",
+    "url": "/portfolio/npm/static/js/runtime-main.1c9edfb5.js"
+  },
+  {
+    "revision": "7721296adf80661179a2390068a484ec",
+    "url": "/portfolio/npm/static/media/projectThumb.7721296a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3eb62a9f54de4d09951fb510231aa2dd.js"
+  "/portfolio/npm/precache-manifest.4f91022de528804db5c79515a51c57bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/npm/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
