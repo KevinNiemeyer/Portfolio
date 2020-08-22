49253105c1473fa97a87da5fdc64aa61b7bc4515
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-08-22 00:31:23 -0500

    Update

--- src/Projects.js
+++ src/Projects.js
@@ -1,4 +1,3 @@
-/* eslint-disable array-callback-return */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Header from './Header.js';
@@ -7,54 +6,55 @@ import styled from 'styled-components';
 import './App.css';
 
 const Container = styled.div`
-	position: relative;
-	width: 100vw;
-	display: flex;
-	flex-direction: column;
-	overflow-y: auto;
-	overflow-x: hidden;
+  position: relative;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  overflow-x: hidden;
 `;
 function Projects(props) {
-	const [ repos, setRepos ] = useState([]);
-	const getData = () => {
-		const url =
-			'https://api.github.com/users/KevinNiemeyer/repos?fbclid=IwAR2stMhMjngpObVKnSVLthIUb6J4oMsix7PmYw_T3kqkWDSuyyNaYvAGWUE';
-		axios
-			.get(url, {
-				private: 'false',
-				fork: 'false'
-			})
-			.then((response) => {
-				let result = response.data;
-				setRepos([ ...repos, ...result ]);
-			});
-	};
-	useEffect(() => {
-		getData();
-	}, []);
-	if (repos.length === 0) {
-		return null;
-	}
-	return (
-		// Render a Thumbnail component
-		<Container>
-			<Header left='3' top='3' scale='3' name='Projects' />
-			{repos.map((repo) => {
-				if (repo.private === false && repo.fork === false) {
-					return (
-						<Project
-							repo={repo}
-							key={repo.id}
-							id={repo.id}
-							git_url={repo.git_url}
-							description={repo.description}>
-							{''}
-						</Project>
-					);
-				}
-			})};
-		</Container>
-	);
+  const [repos, setRepos] = useState([]);
+  const getData = () => {
+    const url =
+      'https://api.github.com/users/KevinNiemeyer/repos?fbclid=IwAR2stMhMjngpObVKnSVLthIUb6J4oMsix7PmYw_T3kqkWDSuyyNaYvAGWUE';
+    axios
+      .get(url, {
+        private: 'false',
+        fork: 'false',
+      })
+      .then((response) => {
+        let result = response.data;
+        setRepos([...repos, ...result]);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  if (repos.length === 0) {
+    return null;
+  }
+  return (
+    // Render a Thumbnail component
+    <Container>
+      <Header left='3' top='3' scale='3' name='Projects' />
+      {repos.map((repo) => {
+        if (repo.private === false && repo.fork === false) {
+          return (
+            <Project
+              repo={repo}
+              key={repo.id}
+              id={repo.id}
+              git_url={repo.git_url}
+              description={repo.description}>
+              {''}
+            </Project>
+          );
+        }
+      })}
+      ;
+    </Container>
+  );
 }
 
 export default Projects;
