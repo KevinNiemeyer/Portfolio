7398c2f715ff6a0b24ac7bd8e5d8795352cac58c
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-08-24 06:27:47 -0500

    Remove homepage for netlify

--- src/App.js
+++ src/App.js
@@ -13,36 +13,39 @@ import Footer from './Footer';
 import NotFound from './NotFound';
 
 const Container = styled.div`
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	margin: 0;
-	padding: 0;
-	transition: all .3s;
-	max-height: 100vh;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  transition: all 0.3s;
+  max-height: 100vh;
 `;
 
 function App() {
-	return (
-		<Router>
-			<Container>
-				<Nav />
-				<Switch>
-					<Route exact path='/' component={Home} />
-					<Route exact path='/home' component={Home} />
-					<Route exact path='/portfolio' component={Home} />
-					<Route exact path='/about' component={About} />
-					<Route exact path='/projects' component={Projects} />
-					<Route exact path='/contact' component={Contact} />
-					<Route path='/*' component={NotFound} />
-				</Switch>
-				<Footer />
-			</Container>
-		</Router>
-	);
+  return (
+    <Router>
+      <Container>
+        <Nav />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/home' component={Home} />
+          <Route exact path='/portfolio' component={Home} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/projects' component={Projects} />
+          <Route exact path='/contact' component={Contact} />
+          <Route path='/*' component={NotFound} />
+        </Switch>
+        <Footer />
+      </Container>
+    </Router>
+  );
 }
 
 export default App;
+
+//belongs in package.json
+//"homepage": " https://KevinNiemeyer.github.io/portfolio/",
