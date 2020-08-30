81ef703f7620cfa2bbbe3bc74d267c6aec8cb3b4
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-08-30 07:17:46 -0500

    Change BrowserRouter to HashRouter for git pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // App.jsx
 
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import styled from 'styled-components';
 import Nav from './Nav';
