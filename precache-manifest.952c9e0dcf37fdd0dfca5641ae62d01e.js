99337c7cc85a0c6247c921b64fbebc8b48ac0cd7
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-09-02 07:18:40 -0500

    Updates

---
+++ precache-manifest.952c9e0dcf37fdd0dfca5641ae62d01e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e6b8c830820f9cf96a0da1617e6e95",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5dbcef842511a61252a1",
+    "url": "/portfolio/static/css/main.aee37114.chunk.css"
+  },
+  {
+    "revision": "6eab792073611f555ce9",
+    "url": "/portfolio/static/js/2.638659be.chunk.js"
+  },
+  {
+    "revision": "be14b386ccc34a49dee76740150eacbc",
+    "url": "/portfolio/static/js/2.638659be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dbcef842511a61252a1",
+    "url": "/portfolio/static/js/main.96c1e590.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f688f77a9bcee72942ca8fefe076aaf2",
+    "url": "/portfolio/static/media/aboutThumb.f688f77a.png"
+  },
+  {
+    "revision": "3e17eee3cb534068f6fce321a5ff547d",
+    "url": "/portfolio/static/media/github-app-preview.3e17eee3.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "9862d452fceaefff5eed47a7174ddf99",
+    "url": "/portfolio/static/media/photogram-preview.9862d452.png"
+  },
+  {
+    "revision": "7721296adf80661179a2390068a484ec",
+    "url": "/portfolio/static/media/projectThumb.7721296a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6f327ea09b2b26857cd888928f49e054.js"
+  "/portfolio/precache-manifest.952c9e0dcf37fdd0dfca5641ae62d01e.js"
 );
 
 self.addEventListener('message', (event) => {
