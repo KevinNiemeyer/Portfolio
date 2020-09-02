54efd59eed8e2c52c4589fc2f2a8dd6a4652bc79
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-09-02 15:45:21 -0500

    Update

--- src/Projects.js
+++ src/Projects.js
@@ -25,7 +25,7 @@ function Projects(props) {
         project_name='Photogram'
         git_url='https://github.com/KevinNiemeyer/photogram'
         live_url='https://kevinniemeyer.github.io/photogram/'
-        description='Instagram clone that consumes the Unsplash API to display and search photos on Unsplash.'
+        description='Instagram clone that consumes the Unsplash API to display and search photos on Unsplash. Created with React.js using React Router, Hooks Styled Components, and Infinite Scroll modules.'
         img_src='./assets/images/photogram-preview.png'></Project>
       <Project
         project_name='GitHub Profile App'
