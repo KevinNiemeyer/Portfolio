9b779683b03a6ba56745a4bb57263c7682d4b655
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-09-02 16:23:54 -0500

    Update

--- src/Projects.js
+++ src/Projects.js
@@ -25,20 +25,26 @@ function Projects(props) {
         project_name='Photogram'
         git_url='https://github.com/KevinNiemeyer/photogram'
         live_url='https://kevinniemeyer.github.io/photogram/'
-        description='Instagram clone that consumes the Unsplash API to display and search photos on Unsplash. Created with React.js using React Router, Hooks Styled Components, and Infinite Scroll modules.'
+        description='Instagram clone that consumes the Unsplash API to display and search photos on Unsplash. Created with create-react-app using React Router, Hooks, Styled Components, Context, Link, and Infinite Scroll modules.'
         img_src='./assets/images/photogram-preview.png'></Project>
       <Project
         project_name='GitHub Profile App'
         git_url='https://github.com/KevinNiemeyer/github-app'
         live_url='https://kevinniemeyer.github.io/github-app/'
-        description='App that uses Axios to retrieve API profile data on GitHub for two profiles and compare their stats'
+        description='My first stab at a React.js app that uses Axios to retrieve API profile data on GitHub for two profiles and compares their stats.'
         img_src='./assets/images/github-app-preview.png'></Project>
       <Project
         project_name='Link Harvester'
         git_url='https://github.com/KevinNiemeyer/link-harvester.git'
         live_url='https://kevinniemeyer.github.io/link-harvester/'
-        description="Harvests all links from a page's HTML code that you copy/paste in or upload from a file. Could be extended to harvest other useful information, such as metadata, tags, etc...  Utilizes my jQuery skills"
+        description="jQuery app that harvests all links from a page's HTML code that you copy/paste in or upload from a file. Could be extended to harvest other useful information, such as metadata, tags, etc... "
         img_src='./assets/images/link-harvester.png'></Project>
+      <Project
+        project_name='Tic Tac Toe'
+        git_url='https://github.com/KevinNiemeyer/tic-tac-toe.git'
+        live_url='https://kevinniemeyer.github.io/tic-tac-toe/'
+        description=''
+        img_src='./assets/images/tic-tac-toe-preview.png'></Project>
     </Container>
   );
 }
