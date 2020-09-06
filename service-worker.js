24ffe73800ec9c0e5b6ba27909b47dc73512355f
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-09-06 10:19:11 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.e9383213371e9e2dfd9377c0cba2b412.js"
+  "/portfolio/precache-manifest.fd196462d4c3dbec81d02901b93a35db.js"
 );
 
 self.addEventListener('message', (event) => {
