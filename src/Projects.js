973dade76799df58a63fc06767b9af6365888549
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-09-09 05:50:38 -0500

    Update

--- src/Projects.js
+++ src/Projects.js
@@ -72,7 +72,7 @@ function Projects(props) {
               git_url='https://github.com/KevinNiemeyer/calculator'
               live_url='https://kevinniemeyer.github.io/calculator/'
               description='My first Bov Academy project using only HTML, CSS, and JavaScript. This was an attempt to clone the iPhone calculator.'
-              img_src='./assets/images/calculator-preview.png'></Project>
+              img_src='#'></Project>
 
             <Project
               project_name='College Brochure'
