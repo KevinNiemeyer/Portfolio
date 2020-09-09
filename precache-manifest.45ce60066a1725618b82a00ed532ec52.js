ad078f6b08ff21563a418784b0cc63849ad36b66
KevinNiemeyer <KevinNiemeyer@users.noreply.github.com>
2020-09-09 05:46:16 -0500

    Updates

--- precache-manifest.45ce60066a1725618b82a00ed532ec52.js
+++ precache-manifest.45ce60066a1725618b82a00ed532ec52.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c9a50bc665eed9e59da69cd014bc2cd7",
+    "revision": "9eea18b1003fc648a4980ca247688dd2",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "256a94160b24d0f74ff1",
+    "revision": "3221902c44e1b8612ca3",
     "url": "/portfolio/static/css/main.d31b78fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/js/2.60b629ce.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "256a94160b24d0f74ff1",
-    "url": "/portfolio/static/js/main.03153f68.chunk.js"
+    "revision": "3221902c44e1b8612ca3",
+    "url": "/portfolio/static/js/main.f137ac5f.chunk.js"
   },
   {
     "revision": "7a6cc99bd8f08f9fa6ec",
@@ -29,7 +29,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "b14b6c7b6bb38f84972e475e555f6eed",
-    "url": "/portfolio/static/media/calculator-preview.b14b6c7b.PNG"
+    "url": "/portfolio/static/media/calculator-preview.b14b6c7b.png"
   },
   {
     "revision": "63b0f72934e5f38083d2a231ba83eba1",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.0721ed09cd107aafae7396b43a3bcc67.js"
+  "/portfolio/precache-manifest.45ce60066a1725618b82a00ed532ec52.js"
 );
 
 self.addEventListener('message', (event) => {
